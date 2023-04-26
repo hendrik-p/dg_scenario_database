66bf67408595b0a328309d9666770ddd7aa5deca
Hendrik <hendrik>
2023-04-26 12:52:02 +0200

    Added missing HTML and Javascript files.

---
+++ dg_scenario_database/static/js/script_tag_browser.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+  const config = {
+    "order": [[1, "asc"]],
+    "pageLength": -1,
+    "lengthMenu": [ [20, 50, 100, -1], [20, 50, 100, "All"] ],
+    "columns": [
+      {data: "id", visible: false},
+      {data: "tag"},
+      {data: "scenarios"},
+      {data: "button"},
+    ]
+  }
+  const table = $('#tag_table').DataTable(config);
+  $('#tag_table').on('click', '.btn', function() {
+    const row = $(this).parents('tr');
+    const name = table.row(row).data()['tag'];
+    const id = table.row(row).data()['id'];
+    console.log(id);
+    $.ajax({
+      url: '/remove_tag_from_database',
+      method: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify({
+        tag_id: id,
+        tag_name: name,
+      }),
+      success: function (response) {
+        if (response.success) {
+          row.remove();
+        } else {
+          console.error('Failed to remove tag from database:', response.message);
+        }
+      },
+      error: function (jqXHR, textStatus, errorThrown) {
+        console.error('AJAX error:', textStatus, errorThrown);
+      }
+    })
+  });
+});
