202237ec499d9c0e952847ac5666b531da47becc
Hendrik <hendrik>
2023-04-28 12:00:38 +0200

    Added database migrations.

--- dg_scenario_database/__init__.py
+++ dg_scenario_database/__init__.py
@@ -5,6 +5,7 @@ import os
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 dictConfig({
     'version': 1,
@@ -30,6 +31,8 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
 
 db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
 
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
